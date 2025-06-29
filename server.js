c53a44f9d28c874f8b76c1307d299c710bfeeabf
H.O.M.E.R <139257311+HackerInRed@users.noreply.github.com>
2025-06-30 04:28:30 +0530

    hmm

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-const fastify = require("fastify")({ logger: true });
-const axios = require("axios");
+const fastify = require('fastify')({ logger: true });
+const axios = require('axios');
+const cors = require('@fastify/cors');
 
-fastify.get("/", async (req, reply) => {
-  reply.send({ message: "Flight proxy is running" });
+fastify.register(cors, {
+  origin: '*',
+  methods: ['GET']
 });
 
-fastify.get("/get-flight", async (req, reply) => {
+fastify.get('/', async (req, reply) => {
+  reply.send({ message: 'Flight proxy is alive' });
+});
+
+fastify.get('/get', async (req, reply) => {
   const { flightNumber, flightDate } = req.query;
 
   if (!flightNumber || !flightDate) {
-    return reply
-      .status(400)
-      .send({ error: "Missing flightNumber or flightDate" });
+    return reply.status(400).send({ error: 'Missing flightNumber or flightDate' });
   }
 
   try {
-    const response = await axios.get(
-      `https://aerodatabox.p.rapidapi.com/flights/number/${flightNumber}/${flightDate}`,
-      {
-        headers: {
-          "X-RapidAPI-Key":
-            "330a15767bmsh691f47052120171p1a403fjsndbc649167a5e",
-          "X-RapidAPI-Host": "aerodatabox.p.rapidapi.com",
-        },
+    const res = await axios.get(`https://aerodatabox.p.rapidapi.com/flights/number/${flightNumber}/${flightDate}`, {
+      headers: {
+        'X-RapidAPI-Key': '330a15767bmsh691f47052120171p1a403fjsndbc649167a5e',
+        'X-RapidAPI-Host': 'aerodatabox.p.rapidapi.com'
       }
-    );
-    reply.send(response.data);
+    });
+
+    reply.send(res.data);
   } catch (err) {
     reply.status(500).send({ error: err.response?.data || err.message });
   }
@@ -33,7 +34,7 @@ fastify.get("/get-flight", async (req, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" });
+    await fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
